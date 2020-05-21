2e3d00122d885c18f62e849360664fe73cf00581
Ruan de Bruyn <rdebruyn@jhb.dvt.co.za>
2020-05-21 19:01:38 +0200

    Added time argument to SecurityCamera class.

--- src/SecurityCamera.py
+++ src/SecurityCamera.py
@@ -3,11 +3,23 @@ from camera.RaspberryCamera import RaspberryCamera
 from runner.TimeRunner import TimeRunner
 from database.S3database import S3database
 from time import sleep
+import argparse
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-t',
+        '--time',
+        type=int,
+        help='Puts security camera on timed mode. Will run for this time before shutting down.',
+        default=60
+    )
+
+    args = parser.parse_args()
+
     cam = RaspberryCamera()
     sleep(1)
-    runner = TimeRunner(10)
+    runner = TimeRunner(args.time)
     s3db = S3database()
     detector = MotionDetector(cam, s3db, runner)
     detector.run()
